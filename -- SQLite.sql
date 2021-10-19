8380782feaca2805202d540d1b9bd3ea17be0bef
Aswathy Ashok <aswathyashok4127@gmail.com>
2021-10-19 16:00:24 +0530

    End of section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id PRIMARY KEY, UserName)
+VALUES (1,"Ash");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Ann");
+
+INSERT INTO Users (Id,UserName)
+VALUES (3,"Sann");
